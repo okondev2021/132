69402f662b9153c9dc90b500166f20003cbd6fb6
unknown <sojapril16@gmail.com>
2023-01-20 20:57:41 +0100

    test2

--- Main_Application/views.py
+++ Main_Application/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
 from . models import User
-from django.http import HttpResponse,JsonResponse,HttpResponseRedirect
-from django.contrib import messages
 from email.message import EmailMessage
 import ssl
 import smtplib
 # Create your views here.
 
-def sending_email(mail,name):
+
+def sending_email(mail, name):
     email_sender = "csc132.collaborators@gmail.com"
     email_password = "avwpgyatfxhdcogw"
     email_receiver = mail
@@ -26,53 +25,17 @@ def sending_email(mail,name):
         smtp.sendmail(email_sender, email_receiver, em.as_string())
 
 def index(request):
-        if request.method == 'POST':
-                username = request.POST['user_name']
-                useremail = request.POST['user_email']
-                user = User.objects.create(Name = username, Email = useremail)
-                print(sending_email(useremail,username))
-                user.save()
-                # messages.info(request,'Thank you, your email has been received. A confirmation email will be sent to you soon')
-                # return HttpResponseRedirect('#form')
-                # if User.objects.filter(Name = username, Email = useremail).exists():
-                #         messages.info(request,'This account already exists')
-                #         return HttpResponseRedirect('#form')
-                # else:
-                #         user = User.objects.create(Name = username, Email = useremail)
-                #         print(sending_email(useremail,username))
-                #         user.save()
-                #         messages.info(request,'Thank you, your email has been received. A confirmation email will be sent to you soon')
-                #         return HttpResponseRedirect('#form')
-        return render(request,'Main_Application/index.html')
-
-def sending_general_email(email_receivers,email_subject,email_body):
-        for email_receiver in email_receivers:
-                email_sender = "csc132.collaborators@gmail.com"
-                email_password = "avwpgyatfxhdcogw"
-                email_receiver = email_receiver.Email
-                subject = email_subject
-                body = email_body
-                em = EmailMessage()
-                em["From"] = email_sender
-                em["To"] = email_receiver
-                em['subject'] = subject
-                em.set_content(body)
-
-                context = ssl.create_default_context()
-                with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
-                        smtp.login(email_sender, email_password)
-                        smtp.sendmail(email_sender, email_receiver, em.as_string())
-
-def admin_message(request):
-        users = User.objects.all()
-        if request.method == 'POST':
-                messageSubject = request.POST['subject']
-                adminmessage =  request.POST['adminmessage'] 
-                # messages.info(request,'Message has been sent')   
-                print(sending_general_email(users,messageSubject,adminmessage))
-        return render(request,'Main_Application/admin-msg.html')
+    if request.method == 'POST':
+        username = request.POST['user_name']
+        email = request.POST['user_email']
+        user = User.objects.create(Name = username, Email = email)
+        print(sending_email(email, username))
+        user.save()
+    return render(request,'Main_Application/index.html')
 
 def csc_admin(request):
     userEmails = User.objects.all()
     return render(request,'Main_Application/csc-admin.html',{'userEmails':userEmails})
 
+def admin_message(request):
+        return render(request,'Main_Application/admin-msg.html')
